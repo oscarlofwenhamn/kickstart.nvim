11193834a6891d06048b9b8e79733ac49f789bba
Oscar Löfwenhamn <oscar.lofwenhamn@gmail.com>
2024-04-07 14:09:03 +0200

    Fix styling

--- init.lua
+++ init.lua
@@ -669,25 +669,35 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'desert'
+      vim.cmd.colorscheme 'catppuccin'
 
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      flavour = 'mocha',
+      -- Alternative way of achieving a broader color change:
+      --[[ color_overrides = {
+        mocha = {
+          overlay0 = '#ffffff',
+          surface1 = '#ffffff',
+        },
+      }, ]]
+      -- Make comments and line numbers a little brighter, to increase visibilty in light conditions
+      highlight_overrides = {
+        mocha = function(mocha)
+          return {
+            Comment = { fg = mocha.overlay2 },
+            LineNr = { fg = mocha.overlay0 },
+          }
+        end,
+      },
+    },
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
